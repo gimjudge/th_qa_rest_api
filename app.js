6350d826b2c371842c8842ade44b2b05da340b09
gimjudge <gimjudge@hotmail.com>
2019-02-02 14:59:52 -0800

    Adding Error handling

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ const app =  express();
 const routes =  require('./routes');
 
 const jsonParser = require("body-parser").json;
+const logger = require("morgan");
 
+app.use(logger("dev"));
 app.use(jsonParser());
 
 app.use("/questions", routes);
 
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
+
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log("Express server is listening on port", port);
 });
